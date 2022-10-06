22dc9b45edf237b86636c61aec49f783ebac9f98
Domdae <phonmongkhon_ph@cmu.ac.th>
2022-10-06 12:28:47 +0700

    first commit

---
+++ src/main/java/com/example/todolistspringboot/TodolistSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.todolistspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodolistSpringbootApplication.class, args);
+    }
+
+}
